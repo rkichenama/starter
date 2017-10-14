f0e8827d7b35bf8eb3416aed1042fcbbfc2dfc30
Bernardo Zuniga <bernardorzuniga@gmail.com>
2017-10-14 00:25:12 -0400

    add jest config and polyfil for r16

--- src/js/app.test.js
+++ src/js/app.test.js
@@ -1,9 +1,5 @@
 import React from 'react';
 
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-configure({ adapter: new Adapter() });
-
 import { shallow } from 'enzyme'
 
 import Hello from './app';
